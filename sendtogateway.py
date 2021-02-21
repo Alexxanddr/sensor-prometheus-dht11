7e5f8dc209753ac57c004dcadbb7a8197586b004
Sottile, Alessandro, Vodafone Italy <Alessandro.Sottile@Vodafone.com>
2021-02-21 10:15:08 +0100

    clean files

--- sendtogateway.py
+++ sendtogateway.py
@@ -15,8 +15,8 @@ def sendData():
     humidity_value=round(dhtDevice.humidity,2)
     
     if humidity_value is not None and temperature_value is not None:
-    	response1 = requests.post('https://pushgateway.sttfv.dns-cloud.net:9444/metrics/job/{j}/instance/{i}/provider/{p}'.format(j=job_name, i=instance_name, p=provider), data='{k} {v}\n'.format(k=temperature_key, v=temperature_value),auth=('asottile', 'iPod1993#'))
-    	response2 = requests.post('https://pushgateway.sttfv.dns-cloud.net:9444/metrics/job/{j}/instance/{i}/provider/{p}'.format(j=job_name, i=instance_name, p=provider), data='{k} {v}\n'.format(k=humidity_key, v=humidity_value),auth=('asottile', 'iPod1993#'))
+    	response1 = requests.post('https://[URL-PROMETHEUS-PUSHGW]:[PORT]/metrics/job/{j}/instance/{i}/provider/{p}'.format(j=job_name, i=instance_name, p=provider), data='{k} {v}\n'.format(k=temperature_key, v=temperature_value),auth=('[BASIC_AUTH_PUSHGW]', '[BASICPASS_AUH_PUSHGW]'))
+    	response2 = requests.post('https://[URL-PROMETHEUS-PUSHGW]:[PORT]/metrics/job/{j}/instance/{i}/provider/{p}'.format(j=job_name, i=instance_name, p=provider), data='{k} {v}\n'.format(k=humidity_key, v=humidity_value),auth=('[BASIC_AUTH_PUSHGW]', '[BASICPASS_AUH_PUSHGW]'))
    
 
 sendData()
